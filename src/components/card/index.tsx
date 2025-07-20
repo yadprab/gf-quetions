8ba2b035d459658ef2563fb9c56393c2ac272f0a
s-ashwin <ashwin.sivanagarajan@gmail.com>
2025-07-20 16:01:02 +0530

    style: :lipstick: Card loader

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -3,15 +3,28 @@ import "./styles.css";
 interface CardProps {
   metrics: string;
   value: string;
+  loading?: boolean;
 }
-export function Card({ metrics, value }: CardProps) {
+
+export function Card({ metrics, value, loading = false }: CardProps) {
   return (
     <div className="card">
       <div>
-        <span className="metrics">{metrics}</span>
-        <div className="value">
-          <strong>{value}</strong>
-        </div>
+        {loading ? (
+          <>
+            <span className="skeleton skeleton-metrics" />
+            <div className="value">
+              <div className="skeleton skeleton-value" />
+            </div>
+          </>
+        ) : (
+          <>
+            <span className="metrics">{metrics}</span>
+            <div className="value">
+              <strong>{value}</strong>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
